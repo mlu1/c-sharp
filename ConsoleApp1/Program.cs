531b2209087e1d9af1316ccd873fd1aa6c152099
mlu@1 <mthandemeluleki@yahoo.com>
2024-08-18 16:25:39 +0800

    continue lesson

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,25 +2,65 @@
 
 namespace HellowWord
 {
+    class Customer{
+        public string firstname;
+        public string  lastName;
+        
+        public void setName(string customerName){
+                firstname = customerName;
+        }
+
+        public void setLastName(string customerLastName){
+            lastName = customerLastName;
+        }
+
+        public string getName(){
+            return this.firstname;
+        }
+
+        public string getLastName(){
+            return this.lastName;
+        }
+
+    }
+
+    class BankAccount{
+        public string BankAccountNumber;
+        public string BankAccountOwner;
+
+        public string Credit(){
+            return "ValueTask";
+        }
+
+        public string Debit(){
+            return "value";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args ){
-        
+            
+            const int vat = 20;
+            const double percentVat = vat/100D;
+            int balance  = 1000;
+
+            Console.WriteLine(balance)
+
             string textAge  = "-23";
             int convertedText = Convert.ToInt32(textAge);
             int age;
-            long bigNumber  = 90000000;
-            decimal floatNumber = 4.09m;
+            long bigNumber  = 90000000L;
+            decimal floatNumber = 4.09M;
             float myFloat = 1.000001F;
             age =50;
             string name = "Mlu";
             char letter = 'a';
             char letter2 = '\0';
-            char letter3 = '\t';
             string textPrecision = "5.00001";
             float precision = Convert.ToSingle(textPrecision);  
             long bigNumber2  = Convert.ToInt32(bigNumber);
-
+            double negative = -55.2D;
             Console.WriteLine("Hello World");
             Console.WriteLine(age);
             Console.WriteLine(bigNumber);
@@ -33,6 +73,9 @@ namespace HellowWord
             Console.WriteLine(letter);
             Console.WriteLine(convertedText);
             Console.WriteLine(precision);
+            Console.WriteLine(negative);
+            Customer customer  = new Customer();
+            BankAccount bankAc = new BankAccount(); 
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681c39f614896bf5f9f8b1d0f28ffe258b12926b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
