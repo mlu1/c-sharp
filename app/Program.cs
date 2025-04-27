cd637aeac350b57c581ee0c5ba5ab3801d2d4bb7
mlu1 <mthandemeluleki@yahoo.com>
2025-04-27 21:33:43 +0800

    normal running app

---
+++ app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace app
+    {
+        class Program
+            {
+                static void Main(string[] args)
+                {    
+                // See https://aka.ms/new-console-template for more information
+                    Console.WriteLine("Hello, World!");
+                    string name = "Yes this is C%";
+                    string mobile = "0900cool";
+                    Console.WriteLine(name);
+                    Console.WriteLine(mobile);
+                    int myNum =10;
+                    Console.WriteLine(myNum);
+                    bool myBool=true;
+                    int a=10,b=20,c=30;
+                    c = a+b+c;
+                    Console.WriteLine(c);
+                }
+            }
+}
+
